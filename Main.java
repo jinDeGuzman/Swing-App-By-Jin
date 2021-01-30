52a637917eb67ac1516e24582841ae43bdf03567
Jin De <91093935@g.mvusd.net>
2021-01-30 05:10:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+//My imports
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Main
+{ 
+  public static void main (String args[])
+  {
+    JFrame frame1 = new JFrame("Jin Book app");
+    frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //Must Have Line
+    frame1.setSize(500, 500);
+    frame1.setVisible(true);
+  }//end main method
+}//end main class
